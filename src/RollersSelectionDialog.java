964782ec8257f09836f764d95536203646381ac1
Lor3nzo-pixel <ctzlnz05e30e514t@isisbernocchi.edu.it>
2023-12-03 11:34:40 +0100

    OK ok ho migliorato la visibilità del codice

--- src/RollersSelectionDialog.java
+++ src/RollersSelectionDialog.java
@@ -3,45 +3,32 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class RollersSelectionDialog extends JDialog implements ActionListener
-{
+public class RollersSelectionDialog extends JDialog implements ActionListener {
+
     public static final int DIALOG_WIDTH = 700, DIALOG_HEIGHT = 400;
-
     private static final int BUTTON_SIZE = 100;
-
     private static final int JTEXTFIELD_SIZE_WIDTH = 80;
-
     private static final int JTEXTFIELD_SIZE_HEIGHT = 15;
-
     private static final int BTN_INCREASE_DECREASE_SIZE = 45;
+    public static final String DIALOG_NAME = "Rollers Configuration";
 
-
-    public static String DIALOG_NAME = "rollers configuration";
-
-
-    private Container c;
+    private Container container;
     private Enigma enigma;
     private Gui owner;
 
-    private JButton[] btnsRollers;
-
-
-    private JTextField txtFieldOffset, txtFieldadvancementOffset;
-
-    private JButton btnIncreaseOffset, btnDecreaseOffSet, btnIncreaseAdvancementOffset, btnDecreaseAdvancementOffSet;
-
+    private JButton[] rollerButtons;
+    private JTextField txtFieldOffset, txtFieldAdvancementOffset;
+    private JButton btnIncreaseOffset, btnDecreaseOffset, btnIncreaseAdvancementOffset, btnDecreaseAdvancementOffset;
     private JPanel panel;
 
-
-    public RollersSelectionDialog(Gui owner)
-    {
+    public RollersSelectionDialog(Gui owner) {
         super(owner, DIALOG_NAME);
         setSize(DIALOG_WIDTH, DIALOG_HEIGHT);
-        setLocation((owner.getWidth() - getWidth())/2, (owner.getHeight() - getHeight())/2);
+        setLocation((owner.getWidth() - getWidth()) / 2, (owner.getHeight() - getHeight()) / 2);
 
         this.owner = owner;
-        c = getContentPane();
-        c.setLayout(null);
+        container = getContentPane();
+        container.setLayout(null);
         enigma = owner.enigma;
 
         createComponents();
@@ -52,92 +39,69 @@ public class RollersSelectionDialog extends JDialog implements ActionListener
         setAutoRequestFocus(true);
 
         setVisible(true);
-
     }
 
-    private void createComponents()
-    {
-
+    private void createComponents() {
         panel = new JPanel(null);
-        panel.setBounds(DIALOG_WIDTH/3, 0, DIALOG_WIDTH/2,DIALOG_HEIGHT/2);
+        panel.setBounds(DIALOG_WIDTH / 3, 0, DIALOG_WIDTH / 2, DIALOG_HEIGHT / 2);
         panel.setBackground(Color.BLUE);
 
-        btnsRollers = new JButton[owner.enigma.getSelectedRollers().length];
+        rollerButtons = new JButton[owner.enigma.getSelectedRollers().length];
 
-        for (int i = 0; i < btnsRollers.length; i++)
-        {
-            btnsRollers[i] = new JButton("Roller "+(i+1));
-            btnsRollers[i].setActionCommand(String.valueOf(i));
-            btnsRollers[i].setBounds(10 + (i * BUTTON_SIZE + 8),10,BUTTON_SIZE,BUTTON_SIZE);
-            btnsRollers[i].setFocusable(false);
-            btnsRollers[i].addActionListener(this);
+        for (int i = 0; i < rollerButtons.length; i++) {
+            rollerButtons[i] = new JButton("Roller " + (i + 1));
+            rollerButtons[i].setActionCommand(String.valueOf(i));
+            rollerButtons[i].setBounds(10 + (i * BUTTON_SIZE + 8), 10, BUTTON_SIZE, BUTTON_SIZE);
+            rollerButtons[i].setFocusable(false);
+            rollerButtons[i].addActionListener(this);
 
-            panel.add(btnsRollers[i]);
+            panel.add(rollerButtons[i]);
         }
 
-        txtFieldOffset = new JTextField(String.valueOf(enigma.getAvailableRoller(0).getCurrentOffset()));
-        txtFieldOffset.setBounds(100, 120, JTEXTFIELD_SIZE_WIDTH, JTEXTFIELD_SIZE_HEIGHT);
-        txtFieldOffset.setEnabled(false);
-        txtFieldOffset.setHorizontalAlignment(JTextField.CENTER);
-        txtFieldOffset.setDisabledTextColor(Color.BLACK);
+        txtFieldOffset = createTextField(enigma.getAvailableRoller(0).getCurrentOffset(), 100, 120);
+        txtFieldAdvancementOffset = createTextField(enigma.getAvailableRoller(0).getAdvancementOffset(),
+                txtFieldOffset.getX() + 80, 120);
 
-        txtFieldadvancementOffset = new JTextField(String.valueOf(enigma.getAvailableRoller(0).getAdvancementOffset()));
-        txtFieldadvancementOffset.setBounds(txtFieldOffset.getX() + 80, 120, JTEXTFIELD_SIZE_WIDTH, JTEXTFIELD_SIZE_HEIGHT);
-        txtFieldadvancementOffset.setEnabled(false);
-        txtFieldadvancementOffset.setDisabledTextColor(Color.BLACK);
-        txtFieldadvancementOffset.setHorizontalAlignment(JTextField.CENTER);
-
-        btnDecreaseOffSet = new JButton("-");
-        btnDecreaseOffSet.setBounds(panel.getX() - 200,150,BTN_INCREASE_DECREASE_SIZE, BTN_INCREASE_DECREASE_SIZE);
-        btnDecreaseOffSet.addActionListener(this);
-        btnDecreaseOffSet.setFocusable(false);
-
-        btnIncreaseOffset = new JButton("+");
-        btnIncreaseOffset.setBounds(btnDecreaseOffSet.getX()+50,150,BTN_INCREASE_DECREASE_SIZE, BTN_INCREASE_DECREASE_SIZE);
-        btnIncreaseOffset.addActionListener(this);
-        btnIncreaseOffset.setFocusable(false);
-
-
-
-        btnDecreaseAdvancementOffSet = new JButton("-");
-        btnDecreaseAdvancementOffSet.setBounds(btnIncreaseOffset.getX()+100,150,BTN_INCREASE_DECREASE_SIZE, BTN_INCREASE_DECREASE_SIZE);
-        btnDecreaseAdvancementOffSet.addActionListener(this);
-        btnDecreaseAdvancementOffSet.setFocusable(false);
-
-        btnIncreaseAdvancementOffset = new JButton("+");
-        btnIncreaseAdvancementOffset.setBounds(btnIncreaseOffset.getX()+150,150,BTN_INCREASE_DECREASE_SIZE,BTN_INCREASE_DECREASE_SIZE);
-        btnIncreaseAdvancementOffset.addActionListener(this);
-        btnIncreaseAdvancementOffset.setFocusable(false);
-
-
+        btnDecreaseOffset = createButton("-", panel.getX() - 200, 150);
+        btnIncreaseOffset = createButton("+", btnDecreaseOffset.getX() + 50, 150);
+        btnDecreaseAdvancementOffset = createButton("-", btnIncreaseOffset.getX() + 100, 150);
+        btnIncreaseAdvancementOffset = createButton("+", btnIncreaseOffset.getX() + 150, 150);
 
         panel.add(txtFieldOffset);
-        panel.add(txtFieldadvancementOffset);
+        panel.add(txtFieldAdvancementOffset);
         panel.add(btnIncreaseOffset);
-        panel.add(btnDecreaseOffSet);
+        panel.add(btnDecreaseOffset);
         panel.add(btnIncreaseAdvancementOffset);
-        panel.add(btnDecreaseAdvancementOffSet);
+        panel.add(btnDecreaseAdvancementOffset);
 
-        c.add(panel);
+        container.add(panel);
+    }
 
+    private JTextField createTextField(int value, int x, int y) {
+        JTextField textField = new JTextField(String.valueOf(value));
+        textField.setBounds(x, y, JTEXTFIELD_SIZE_WIDTH, JTEXTFIELD_SIZE_HEIGHT);
+        textField.setEnabled(false);
+        textField.setHorizontalAlignment(JTextField.CENTER);
+        textField.setDisabledTextColor(Color.BLACK);
+        return textField;
+    }
 
+    private JButton createButton(String label, int x, int y) {
+        JButton button = new JButton(label);
+        button.setBounds(x, y, BTN_INCREASE_DECREASE_SIZE, BTN_INCREASE_DECREASE_SIZE);
+        button.addActionListener(this);
+        button.setFocusable(false);
+        return button;
     }
 
     @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        if (e.getSource() == btnIncreaseOffset || e.getSource() == btnDecreaseOffSet)
-        {
-
-        }
-        else
-        {
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnIncreaseOffset || e.getSource() == btnDecreaseOffset) {
+            // Handle increase/decrease offset actions
+        } else {
             int index = Integer.parseInt(e.getActionCommand());
-
             txtFieldOffset.setText(String.valueOf(enigma.getAvailableRoller(index).getCurrentOffset()));
-            txtFieldadvancementOffset.setText(String.valueOf(enigma.getAvailableRoller(index).getAdvancementOffset()));
+            txtFieldAdvancementOffset.setText(String.valueOf(enigma.getAvailableRoller(index).getAdvancementOffset()));
         }
-
-
     }
 }
